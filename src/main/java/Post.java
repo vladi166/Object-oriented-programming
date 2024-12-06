1d6669ff667c0529f5932ae505258b4cac73fc8e
Vladislav Andreev <vladireft96@mail.ru>
2024-12-06 18:19:27 +0500

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,11 @@
+public class Post {
+
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String surname;
+    public String phone;
+    public boolean subscription;
+
+    FormDate birthday = new FormDate();
+}
